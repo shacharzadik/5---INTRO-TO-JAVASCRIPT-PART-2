2b79a6f520ce7a06b26a75ca788aa2742a684248
שחר <shachar@shrs-MacBook-Air.local>
2017-12-17 20:51:08 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+/* var age = prompt('What is your age?');
+
+if (age < 18) {
+    alert('Sorry,the bar is closed for you kiddo');
+} else {
+    alert('Cool! What would you like to drink?');
+} 
+
+var citizenship = prompt('What is your citizenship?');
+
+if (citizenship === "Israeli") {
+    alert('What a beautiful day!, welcome hom!');
+
+} else {
+    alert("Sorry you can't enter our country!");
+}
+*/
+
+
+/* If excercise 
+var gender = prompt("What is your gender?");
+var age = prompt("What is your age?");
+
+if (gender==="Female" && age >=18) {
+    alert("Cool! you can have a free drink!");
+}
+ else if (age<18) {
+    alert("You can't drink anything");
+}
+ else {
+    alert("You can drink but have to pay");
+}
+*/
+
+var restaurants = ["Shemesh", "Oved", "Zepra", "Girrafe" , "Cafe Italia"];
+
+for (number=0; number<=restaurants.length; number++) {
+    console.log(restaurants[number]);
+}
+
+
